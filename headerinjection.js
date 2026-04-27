c7f7ab2c27bf0e3f2df72427df109b0ae0e4134d
yalspameni <student.ls2@youngsvilleacademy.org>
2026-04-27 12:59:59 -0400

    Create headerinjection.js

---
+++ headerinjection.js
@@ -0,0 +1,8 @@
+let elem = document.createElement("div");
+elem.innerHTML = " <link rel='stylesheet'href='https://yalspameni.github.io/web-dev/header.css'>";
+document.body.prepend(elem);
+
+fetch("https://yalspameni.github.io/web-dev/globalheader.html")
+.then((result) => result.text())
+.then((text) => {elem.innerHTML = elem.innerHTML + text;})
+.catch((e) => console.error(e));
